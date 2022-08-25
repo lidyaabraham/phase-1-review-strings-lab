4ef0707bb53a1cb3d1ce3050aad691cbc9a8c459
lidya <lidya. abraham@student.moringaschool.com>
2022-08-25 14:25:39 +0300

    commpleted assignment

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Write your code in this file!
-const currentUser = "Grace Hopper";
-const welcomeMessage = "Welcome to Flatbook!"; 
-
+//The 'currentUser' variable must contain a string
+const currentUser = 'Grace Hopper';
+//contains "Welcome to Flatbook
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+//contains excitedWelcomeMessage-toUpperCas
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-currentUser.slice(0, 1);
+//shortGreeting
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
